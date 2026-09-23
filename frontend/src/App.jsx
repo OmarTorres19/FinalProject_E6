efe300a58b5f4a02f9459d1feabec8bbee89b672
SVINOUN <rodrigogvzl6@gmail.com>
2026-09-22 20:31:33 -0600

    Instalaciòn y creación del entorno base de React con Vite (ESLite) para el frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+// El componente App es el contenedor principal de toda tu aplicación
+function App() {
+  return (
+    <div>
+      <h1>Sistema de Gestión de Usuarios</h1>
+      <p>El entorno de React está listo para programar.</p>
+      {/* Aquí llamaremos a nuestro componente Login más adelante */}
+    </div>
+  );
+}
+
+// Exportamos el componente para que main.jsx pueda renderizarlo
+export default App;
